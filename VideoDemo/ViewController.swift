54273a8e14030576232df04856e7965c5bb0df76
YahiaMosaad <YahiaMosaad>
2019-11-18 18:57:15 +0400

    Initial Commit

---
+++ VideoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoDemo
+//
+//  Created by Yahia Mosaad on 11/18/19.
+//  Copyright © 2019 YahiaMosaad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
